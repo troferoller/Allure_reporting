066ce19c3db3bee6bbebdc3316073992865577ad
troferoller <d.dulger@mail.ru>
2024-01-29 21:17:57 +0300

    reworked SelenideTest

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.withText;
@@ -7,15 +9,19 @@ import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.By.linkText;
 
 public class SelenideTest extends TestBase {
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 80;
+
     @Test
     public void selenideTest(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
         open("");
         $("[data-target='qbsearch-input.inputButtonText']").click();
-        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").sendKeys(REPOSITORY);
         $("#query-builder-test").submit();
 
-        $(linkText("eroshenkoam/allure-example")).click();
+        $(linkText(REPOSITORY)).click();
         $("#issues-tab").click();
-        $(withText("#80")).should(Condition.exist);
+        $(withText("#"+ ISSUE)).should(Condition.exist);
     }
 }
